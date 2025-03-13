b4766db862a0bf08eff3f4a2ec967dfaed87c90a
David Gardiner <david@gardiner.net.au>
2025-03-13 21:40:55 +1030

    pass in env

--- legacy/main.tf
+++ legacy/main.tf
@@ -15,12 +15,14 @@ provider "azurerm" {
 
 module "plan" {
   source                  = "./modules/plan"
+  environment             = var.environment
   resource_group_name     = data.azurerm_resource_group.group.name
   resource_group_location = data.azurerm_resource_group.group.location
 }
 
 module "web" {
   source                  = "./modules/web"
+  environment             = var.environment
   resource_group_name     = data.azurerm_resource_group.group.name
   resource_group_location = data.azurerm_resource_group.group.location
   app_service_plan_id     = module.plan.app_service_plan_id
