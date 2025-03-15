dcd262f4cdf859d23937c85bca45a4ddb8e7bf9b
David Gardiner <david@gardiner.net.au>
2025-03-15 12:00:04 +1030

    Add footer with page count

---
+++ global-bottom.vue
@@ -0,0 +1,10 @@
+<!-- global-bottom.vue -->
+<template>
+    <footer class="absolute bottom-0 left-0 right-0 p-2">David Gardiner</footer>
+    <footer
+    v-if="$nav.currentLayout !== 'cover'"
+    class="absolute bottom-0 right-0 p-2"
+  >
+    {{ $nav.currentPage }} / {{ $nav.total }}
+    </footer>
+</template>
