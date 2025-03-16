df856fb5b824a4d0766ba7e366405c6a79753786
David Gardiner <david@gardiner.net.au>
2025-03-16 15:07:35 +1030

    Without KV access policy

--- terraform/key-vault.tf
+++ terraform/key-vault.tf
@@ -29,14 +29,14 @@ import {
   to = azurerm_key_vault_access_policy.pipeline_spn[0]
 }
 
-resource "azurerm_key_vault_access_policy" "pipeline_spn" {
-  count = contains(["dev", "prod"], var.environment) ? 1 : 0
+# resource "azurerm_key_vault_access_policy" "pipeline_spn" {
+#   count = contains(["dev", "prod"], var.environment) ? 1 : 0
 
-  key_vault_id            = azurerm_key_vault.kv.id
-  tenant_id               = data.azurerm_client_config.current.tenant_id
-  object_id               = data.azurerm_client_config.current.object_id
-  certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "ManageContacts", "ManageIssuers", "GetIssuers", "ListIssuers", "SetIssuers", "DeleteIssuers", "Purge"]
-  secret_permissions      = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"]
-  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"]
-  storage_permissions     = ["Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update"]
-}
+#   key_vault_id            = azurerm_key_vault.kv.id
+#   tenant_id               = data.azurerm_client_config.current.tenant_id
+#   object_id               = data.azurerm_client_config.current.object_id
+#   certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "ManageContacts", "ManageIssuers", "GetIssuers", "ListIssuers", "SetIssuers", "DeleteIssuers", "Purge"]
+#   secret_permissions      = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"]
+#   key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"]
+#   storage_permissions     = ["Backup", "Delete", "DeleteSAS", "Get", "GetSAS", "List", "ListSAS", "Purge", "Recover", "RegenerateKey", "Restore", "Set", "SetSAS", "Update"]
+# }
