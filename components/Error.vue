eb052ea8484ad99a5b72ed28bc2a2f0ffe845ed6
David Gardiner <david@gardiner.net.au>
2025-06-04 18:11:34 +0930

    Center error heading

--- components/Error.vue
+++ components/Error.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="error">
-    <h1 v-if="props.text">
-    <img src="/error-icon-25240.png" alt="Error Icon" :align=props.align valign="bottom" :style="{ verticalAlign: 'baseline' }" /> 
-    {{ props.text }}
-  </h1>
+    <h1 v-if="props.text" class="error-heading">
+      <img src="/error-icon-25240.png" alt="Error Icon" />
+      <span>{{ props.text }}</span>
+    </h1>
     <br style="clear: both;" />
   </div>
 </template>
 
 <script lang="ts" setup>
-
 export interface Props {
   text: string;
   align?: 'left' | 'right' | 'center';
@@ -18,13 +17,17 @@ export interface Props {
 const props = withDefaults(defineProps<Props>(), {
 
 });
-
 </script>
 
 <style scoped>
 .error {
   color: red;
   font-weight: bold;
+}
+.error-heading {
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
 }
 .error img {
   width: 64px;
